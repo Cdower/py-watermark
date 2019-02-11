f2493318e985f76b5d7903e48d114cbafdfcf586
Chris Dower <cliffjumpers57@gmail.com>
2019-02-10 18:44:20 -0800

    open and save image

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Watermark one or more images.')
     parser.add_argument('--watermark-text', nargs=1,
         help="String of watermark text, in quotes")
-    parser.add_argument('--output-dir', nargs=1, type=argparse.FileType('w'),
-        default=sys.stdin, help="Output dir of watermarked image(s)")
-    parser.add_argument('--infile', nargs='?', type=argparse.FileType('r'),
-        default=sys.stdin, help="Image(s) to watermark")
+    parser.add_argument('--watermark-image', nargs=1, type=argparse.FileType('r'),
+        help="Water Mark Image")
+    parser.add_argument('--output-dir', nargs=1, default='./watermarked-images',
+        help="Output dir of watermarked image(s)") #type=argparse.FileType('w'),
+    parser.add_argument('--infile', nargs='?', help="Image(s) to watermark", required=True)
     #
     #
     args = parser.parse_args()
-    print(args)
+    #print(args)
+    watermark_in = {
+        "text": str().join(args.watermark_text),
+        "image": args.watermark_image
+    }
+    watermark(images=args.infile, watermark=watermark_in, output_dir=args.output_dir[0])
--- watermark.py
+++ watermark.py
@@ -1,21 +1,49 @@
-import os,sys
-from PIL import Image
+import os,sys,io
+import logging
+from PIL import Image, ImageFont, ImageDraw
 
 '''
 Takes a single files, list of files or a directory and returns a list of files
 '''
-def gather_files(passed_argv):
-    return []
+def gather_files(images):
+    return [images]
 
-def watermark(passed_argv):
-    #passed_argv[1:]
-    files_to_watermark = gather_files(passed_argv[1:])
+def watermark(images, watermark, output_dir, opacity=50):
+    #log level debug: 
+    # print(images)
+    files_to_watermark = gather_files(images)
     #TODO: Parse Directories to List of Files
     #if os.path.isdir(passed_argv[1]):
     #    print('its a path')
     for infile in files_to_watermark:
         try:
-            with Image.open(infile) as im:
+            with Image.open(infile).convert('RGBA') as im:
                 print(infile, im.format, "%dx%d" % im.size, im.mode)
+
+                txt = Image.new('RGBA', im.size, (255,255,255,0))
+
+                draw = ImageDraw.Draw(txt)
+                font = ImageFont.truetype('./Open_Sans/OpenSans-Regular.ttf', 72)
+                start = (20,20)
+                fill = (255,255,255,int(opacity/100 * 255))
+                print(watermark["text"])
+                draw.text(start, watermark["text"], font=font, fill=fill)
+                
+                out = Image.alpha_composite(im,txt)
+                out.show()
+                if not os.path.isdir(output_dir):
+                    os.mkdir(output_dir)
+                file_split = os.path.basename(infile).split('.')
+                file_split[-1]='png'
+                output_file = output_dir +'/'+'.'.join(file_split)
+                out.save(output_file, format="PNG", optimize=True)
+                out.thumbnail( ( int(im.size[0]*.2), int(im.size[1]*.2)) )
+                file_split = os.path.basename(infile).split('.')
+                file_split.append(file_split[-1])
+                file_split[-2]='thumbnail'
+                file_split[-1]='png'
+                output_file = output_dir +'/'+ '.'.join(file_split)
+                out.save(output_file, 'PNG', optimize=True)
         except IOError:
-            pass
+            # Log Failed to open infile
+            pass    
